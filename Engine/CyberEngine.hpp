4308b67bab051915bc1b069b8f39a8a17acd82fc
Cyberboss <Cyberboss@users.noreply.github.com>
2016-03-21 12:01:54 -0400

    Defined the startup sequence. Defined API namespace.

--- Engine/CyberEngine.hpp
+++ Engine/CyberEngine.hpp
@@ -1,6 +1,15 @@
 //! @file CyberEngine.hpp Precompiled header for engine API
 #pragma once
 
+//! @brief Encapsulates the engine in it's entirety
+namespace CYB {
+	//! @brief Namespace containing the public API define EXPLICIT_API_USAGE to prevent automatic 'using'
+	namespace API {};
+#ifndef EXPLICIT_CYB_API_USAGE
+	using namespace API;
+#endif
+};
+
 #include "Preprocessor.hpp"
 
 #include "STL.hpp"

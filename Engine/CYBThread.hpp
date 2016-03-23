74351b487013c64b69937636ac1a603b0f4ad8f3
Cyberboss <Cyberboss@users.noreply.github.com>
2016-03-23 13:37:46 -0400

    Wrote the threading interface

--- Engine/CYBThread.hpp
+++ Engine/CYBThread.hpp
@@ -5,7 +5,25 @@ namespace CYB {
 		//! @brief Definition of a platform thread object
 		class Thread : private Implementation::Thread {
 		public:
+			//! @brief The states of a thread
+			enum class State {
+				IDLE,		//!< @brief The thread is not running and can be started
+				RUNNING,	//!< @brief The thread is running
+				FINISHED,	//!< @brief The thread has finished running
+			};
+		private:
+			API::Threadable& FThreadable;	//!< @brief A reference to the Threadable object this Thread will run
+		public:
+			/*!
+				@brief Construct a Thread. Once returned, the thread will be scheduled to run
+				@param AThreadable A reference to the threadable object the Thread will run
+			*/
+			Thread(API::Threadable& AThreadable);
+			~Thread();
 
+			void Cancel(void);
+			bool GetState(void) const;
+			void Wait(void) const;
 		};
 	};
 };

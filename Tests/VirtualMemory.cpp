69f55543bc2ceb653ddeb730902915109215d615
Cyberboss <Cyberboss@users.noreply.github.com>
2016-04-28 16:38:36 -0400

    Memory bug fixes. More VM testing
    Related Work Items: #13, #14

--- Tests/VirtualMemory.cpp
+++ Tests/VirtualMemory.cpp
@@ -5,8 +5,17 @@ SCENARIO("VirtualMemory reservations handle various sizes", "[VirtualMemory]") {
 		unsigned long long ReservationSize;
 		WHEN("The size is sane") {
 			ReservationSize = 1000000;
+			void* Result(nullptr);
+			bool Error(false);
+			try {
+				Result = CYB::Platform::VirtualMemory::Reserve(ReservationSize);
+			}
+			catch (...) {
+				Error = true;
+			}
 			THEN("The range is reserved and result is not null") {
-				REQUIRE(CYB::Platform::VirtualMemory::Reserve(ReservationSize) != nullptr);
+				REQUIRE(Result != nullptr);
+				REQUIRE(!Error);
 			}
 		}
 		WHEN("The size is insanely small") {
@@ -24,6 +33,7 @@ SCENARIO("VirtualMemory reservations handle various sizes", "[VirtualMemory]") {
 				REQUIRE(Result == nullptr);
 			}
 		}
+#ifndef TARGET_OS_LINUX
 		WHEN("The size is insanely large") {
 			ReservationSize = std::numeric_limits<unsigned long long>().max();
 			bool Error(false);
@@ -39,5 +49,52 @@ SCENARIO("VirtualMemory reservations handle various sizes", "[VirtualMemory]") {
 				REQUIRE(Result == nullptr);
 			}
 		}
+#endif
+	}
+}
+SCENARIO("VirtualMemory reservations handle various commit sizes", "[VirtualMemory]") {
+	GIVEN("A standard reservation") {
+		auto Reservation(CYB::Platform::VirtualMemory::Reserve(1000000));
+		unsigned long long CommitSize;
+		WHEN("The size is normal") {
+			CommitSize = 4096;
+			bool Error(false);
+			try {
+				CYB::Platform::VirtualMemory::Commit(Reservation, CommitSize);
+			}
+			catch (...) {
+				Error = true;
+			}
+			THEN("The commit works as normal") {
+				REQUIRE(!Error);
+			}
+		}
+		WHEN("The size is insanely small") {
+			CommitSize = 1;
+			bool Error(false);
+			try {
+				CYB::Platform::VirtualMemory::Commit(Reservation, CommitSize);
+			}
+			catch (...) {
+				Error = true;
+			}
+			THEN("The commit works as normal, but nothing changes") {
+				REQUIRE(!Error);
+			}
+		}
+		WHEN("The size is larger than the reservation") {
+			CommitSize = 2000000;
+			bool Error(false);
+			try {
+				CYB::Platform::VirtualMemory::Commit(Reservation, CommitSize);
+			}
+			catch (CYB::Exception::SystemData AException) {
+				Error = AException.FErrorCode == CYB::Exception::SystemData::MEMORY_COMMITAL_FAILURE;
+			}
+			THEN("The commit fails") {
+				REQUIRE(Error);
+			}
+		}
+		CYB::Platform::VirtualMemory::Release(Reservation);
 	}
 }

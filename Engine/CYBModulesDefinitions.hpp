a8094928b5849a60d6b68d9a1e7fbb48469d0836
Cyberboss <cyberboss1994@hotmail.com>
2016-05-02 16:30:58 -0400

    Cleaned up header organization. Removed the accidentally commited CYB.i

---
+++ Engine/CYBModulesDefinitions.hpp
@@ -0,0 +1,13 @@
+//! @file CYBModuleDefinitions.inl Defines needed modules used by the platform layer
+
+namespace CYB {
+	namespace Engine {
+		template <unsigned int AN, typename... AFunctionTypes> class AutoModule;
+	};
+};
+
+#ifndef DOXY
+
+
+
+#endif

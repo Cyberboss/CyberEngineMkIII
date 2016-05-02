e7114bee4e4489eddb12f52823e668ae3727d105
Cyberboss <Cyberboss@users.noreply.github.com>
2016-05-02 16:33:34 -0400

    Fixed the newer WinAPIs not showing up

--- Engine/CYBWin32Implementation.hpp
+++ Engine/CYBWin32Implementation.hpp
@@ -4,7 +4,8 @@ namespace CYB {
 	namespace Platform {
 		namespace Implementation {
 			namespace Win32 {
-				#define WIN32_LEAN_AND_MEAN
+				#define WINBLUE_KBSPRING14	//updated APIs
+				#define WIN32_LEAN_AND_MEAN	//kiss
 				#include <Windows.h>
 				#undef WIN32_LEAN_AND_MEAN
 				#undef Yield

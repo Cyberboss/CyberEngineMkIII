0d2591fc767b62bfef8a26a7dbd79441681d85cc
Cyberboss <cyberboss1994@hotmail.com>
2016-05-03 16:53:27 -0400

    Defined the heap interface

---
+++ Engine/Heap.hpp
@@ -0,0 +1,39 @@
+//! @file Heap.hpp Defines the Heap interface
+namespace CYB {
+	namespace API {
+		//! @brief An interface which can allocate memory
+		class Heap {
+		public:
+			/*!
+				@brief Allocate memory from the heap for use
+				@param ASize The amount of bytes to allocate
+				@return The beginning of an allocated memory region with size at least @p ASize
+				@par Thread Safety
+					This function requires no thread safety
+				@par Exception Safety
+					CYB::Exception::SystemData::MEMORY_COMMITAL_FAILURE if the heap does not have the space for the allocation and more system memory cannot be requested
+			*/
+			virtual void* Alloc(const unsigned long long ASize) = 0;
+			/*!
+				@brief Allocate memory from the heap for use while preserving previous data
+				@param APreviousAllocation A pointer previously returned from a call to Alloc or Realloc of the same Heap object
+				@param ANewSize The amount of bytes to allocate
+				@return The beginning of an allocated memory region with size at least @p ANewSize and the data from @p APreviousAllocation
+				@par Thread Safety
+					This function requires no thread safety
+				@par Exception Safety
+					CYB::Exception::SystemData::MEMORY_COMMITAL_FAILURE if the heap does not have the space for the allocation and more system memory cannot be requested
+			*/
+			virtual void* Realloc(void* const APreviousAllocation, const unsigned long long ANewSize) = 0;
+			/*!
+				@brief Return memory to the heap. Data in allocated range will be lost
+				@param APreviousAllocation A pointer previously returned from a call to Alloc or Realloc of the same Heap object
+				@par Thread Safety
+					This function requires no thread safety
+				@par Exception Safety
+					This function does not throw exceptions
+			*/
+			virtual void* Free(void* const APreviousAllocation) = 0;
+		};
+	};
+};

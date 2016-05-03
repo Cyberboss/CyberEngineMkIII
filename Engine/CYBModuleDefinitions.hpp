b78d9fcaf70f96651e4df84d06b57a755b102ed0
Cyberboss <cyberboss1994@hotmail.com>
2016-05-03 16:09:50 -0400

    Removed dummy module definition

--- Engine/CYBModuleDefinitions.hpp
+++ Engine/CYBModuleDefinitions.hpp
@@ -4,21 +4,6 @@
 
 #ifdef TARGET_OS_WINDOWS
 DEFINE_MODULE(Kernel32, Win32, true, DiscardVirtualMemory)
-#else
-namespace CYB {
-	namespace Platform {
-		namespace Implementation {
-			namespace Posix {
-				void* SomeFakeFunction(void* AA, int AV, pthread_t* c);
-			};
-		};
-	};
-};
-DEFINE_MODULE(FakeAF, Posix, true, SomeFakeFunction)
 #endif
-
-
-
-
 
 #endif

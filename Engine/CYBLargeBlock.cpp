be776087452f49e250addc82e6a74eb6db9dcf6f
Cyberboss <Cyberboss@users.noreply.github.com>
2016-05-06 15:47:22 -0400

    Wrote some LargeBlock helper functions

--- Engine/CYBLargeBlock.cpp
+++ Engine/CYBLargeBlock.cpp
@@ -4,6 +4,14 @@
 #pragma warning(disable : 4355) //this used in initalizer list
 #endif
 
+static_assert(sizeof(CYB::Engine::LargeBlock) - sizeof(CYB::Engine::Block) ==
+#ifdef DEBUG
+	32
+#else
+	16
+#endif
+	, "LargeBlock size has changed, check algorithms");
+
 CYB::Engine::LargeBlock::LargeBlock(const unsigned long long ASpaceAvailable) :
 	Block(0, *this, true),
 	FMagicHeader(MAGIC_HEADER),
@@ -11,3 +19,28 @@ CYB::Engine::LargeBlock::LargeBlock(const unsigned long long ASpaceAvailable) :
 	FRemainingSize(ASpaceAvailable),
 	FMagicFooter(MAGIC_FOOTER)
 {}
+CYB::Engine::LargeBlock::LargeBlock(const unsigned long long ASpaceAvailable, LargeBlock* ALargeBlockToTheRight) :
+	Block(0, *this, true),
+	FMagicHeader(MAGIC_HEADER),
+	FLargeBlockToTheRight(ALargeBlockToTheRight),
+	FRemainingSize(ASpaceAvailable),
+	FMagicFooter(MAGIC_FOOTER)
+{}
+
+CYB::Engine::Block* CYB::Engine::LargeBlock::AllocateBlock(LargeBlock*& ALargeBlock, const int ANewBlockSize) {
+	const auto SizeWithBlock(ANewBlockSize + sizeof(Block));
+	const auto OriginalSize(ALargeBlock->FRemainingSize);
+	if (OriginalSize > SizeWithBlock) {
+		ALargeBlock->SetSize(ANewBlockSize);
+		ALargeBlock->SetFree(false);
+		Block* NewBlock(ALargeBlock);
+		ALargeBlock = new (ALargeBlock->GetData()) LargeBlock(OriginalSize - SizeWithBlock, ALargeBlock->FLargeBlockToTheRight);
+		return NewBlock;
+	}
+	else
+		return nullptr;
+}
+
+CYB::Engine::Block* CYB::Engine::LargeBlock::RightBlock(void) {
+	return reinterpret_cast<Block*>(reinterpret_cast<byte*>(this + 1) + FRemainingSize);
+}
--- Engine/CYBLargeBlock.hpp
+++ Engine/CYBLargeBlock.hpp
@@ -4,16 +4,41 @@ namespace CYB {
 	namespace Engine {
 		//! @brief Used to identify the end of a Heap
 		class LargeBlock : public Block {
-		private:
 #ifdef DEBUG	//Block debug headers
+		private:
 			unsigned long long FMagicHeader; //!< @brief A guard against memory corruption
 #endif
+		public:
 			LargeBlock* FLargeBlockToTheRight; //!< @brief Pointer to a large block in a higher memory location in the current reservation
+		private:
 			unsigned long long FRemainingSize; //!< @brief The amount of space the block has available
 #ifdef DEBUG
 			unsigned long long FMagicFooter; //!< @brief A guard against memory corruption
 #endif
+		private:
+			/*!
+				@brief Allocation constructor. Should be used with operator new in a memory location at least @p ASpaceAvailable large. The LargeBlock will own this memory
+				@param ASpaceAvailable The amount of space the large block is using
+				@param ALargeBlockToTheRight The allocated LargeBlock's value of FLargeBlockToTheRight
+				@par Thread Safety
+					This function requires that the owned memory isn't concurrently accessed during this function
+				@par Exception Safety
+					This function does not throw exceptions
+			*/
+			LargeBlock(const unsigned long long ASpaceAvailable, LargeBlock* ALargeBlockToTheRight);
 		public:
+			/*!
+				@brief Allocate a portion of the LargeBlock's owned data to create a regular block
+				@param[out] ALargeBlock A reference to the pointer pointing to the LargeBlock from which the new Block will be allocated. Will be changed to the LargeBlock's new location upon return
+				@param ANewBlockSize The size of the Block to be allocated
+				@return A new Block with the correct size and free state if it succeeded. Nullptr will be return in the other case of not having enough space in @p ALargeBlock
+				@par Thread Safety
+					This function requires that the owned memory isn't concurrently accessed during this function
+				@par Exception Safety
+					This function does not throw exceptions
+			*/
+			static Block* AllocateBlock(LargeBlock*& ALargeBlock, const int ANewBlockSize);
+
 			/*!
 				@brief Initialization constructor. Should be used with operator new in a memory location at least @p ASpaceAvailable large that extends to the end of a reservation. The LargeBlock will own this memory
 				@param ASpaceAvailable The amount of space the large block is using
@@ -23,6 +48,18 @@ namespace CYB {
 					This function does not throw exceptions
 			*/
 			LargeBlock(const unsigned long long ASpaceAvailable);
+			/*!
+				@brief Expand the capacity of the LargeBlock. If this is not the rightmost block, the Block to the right will be notified to update it's previous offset. If the new Block to the right is another LargeBlock, it will be merged into this one
+				@param AAmountToAdd The size to add to the LargeBlock
+				@par Thread Safety
+					This function requires that the owned memory isn't concurrently accessed during this function
+				@par Exception Safety
+					This function does not throw exceptions
+			*/
+			void Expand(const unsigned long long AAmountToAdd);
+
+			//! @brief See CYB::Engine::Block::RightBlock. This version must be used when using large blocks
+			Block* RightBlock(void);
 		};
 	};
 }; 

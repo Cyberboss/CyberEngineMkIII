5e824c968d81608abe05557cf1ede03e65cfc5b4
Cyberboss <cyberboss1994@hotmail.com>
2016-06-01 16:34:12 -0400

    Basic heap test

--- Tests/Engine/Memory/Heap.cpp
+++ Tests/Engine/Memory/Heap.cpp
@@ -2,24 +2,22 @@
 
 using namespace CYB::Engine::Memory;
 
-static unsigned int* FAllocationFunctionCalled;
-
-REDIRECTED_FUNCTION(VirtualAlloc, void* const AAddress, const unsigned long long ASize, const unsigned long AAllocationType, const unsigned long AProtection) {
-	++(*FAllocationFunctionCalled);
-	return ARedirector::CallOriginal(AAddress, ASize, AAllocationType, AProtection);
-}
-
-REDIRECTED_FUNCTION(MMap, void* const AAddress, const unsigned long long ASize, int AProtection, int AFlags, int AFD, const unsigned long long AOffset) {
-	++(*FAllocationFunctionCalled);
-	return ARedirector::CallOriginal(AAddress, ASize, AProtection, AFlags, AFD, AOffset);
-}
-
-SCENARIO("Test basic Heap functions", "[Engine][Memory][Functional]") {
+SCENARIO("Test Heap Alloc", "[Engine][Memory][Functional]") {
 	ModuleDependancy<CYB::API::Platform::WINDOWS, CYB::Platform::Modules::AMKernel32> K32(CYB::Core().FModuleManager.FK32);
 	ModuleDependancy<CYB::API::Platform::POSIX, CYB::Platform::Modules::AMLibC> LibC(CYB::Core().FModuleManager.FC);
-	GIVEN("A basic Heap") {
-		auto VA(K32.Redirect<CYB::Platform::Modules::Kernel32::VirtualAlloc, VirtualAlloc>());
-		auto MM(LibC.Redirect<CYB::Platform::Modules::LibC::mmap, MMap>());
-		REQUIRE(true);
+	GIVEN("A basic heap") {
+		Heap TestHeap(10000);
+		WHEN("A sane allocation is made") {
+			void* Result(nullptr);
+			REQUIRE_NOTHROW(Result = TestHeap.Alloc(50));
+			THEN("A valid, allocated, aligned memory region is returned") {
+				CHECK(Result != nullptr);
+				CHECK_FALSE(Block::FromData(Result).IsLargeBlock());
+				CHECK_FALSE(Block::FromData(Result).IsFree());
+				CHECK(Block::FromData(Result).Size() >= 50);
+				CHECK(Block::FromData(Result).Size() % sizeof(void*) == 0U);
+				CHECK(reinterpret_cast<unsigned long long>(Result) % sizeof(void*) == 0U);
+			}
+		}
 	}
 }

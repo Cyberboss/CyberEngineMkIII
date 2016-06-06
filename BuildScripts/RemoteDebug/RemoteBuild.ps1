2da49a8c2564774e3734fa073729769a5d3cd610
Cyberboss <cyberboss1994@hotmail.com>
2016-06-06 15:17:43 -0400

    noop change

--- BuildScripts/RemoteDebug/RemoteBuild.ps1
+++ BuildScripts/RemoteDebug/RemoteBuild.ps1
@@ -16,4 +16,3 @@ if($LastExitCode -eq 0){
 	$command = "cd $wd; cd ../..;cmake . -DCMAKE_BUILD_TYPE=DEBUG -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1;dos2unix Tests/llvm-gcov.sh;chmod +x Tests/llvm-gcov.sh;make && make test CTEST_OUTPUT_ON_FAILURE=1 && make Code.coveragexml"
 	& "BuildScripts/RemoteDebug/plink.exe" "$userhost" -i "$key" "$command"
 }
-

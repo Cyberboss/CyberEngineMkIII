1b536d5e10d46ea4a0437ec1eae2ad7a19c30a91
Cyberboss <Cyberboss@users.noreply.github.com>
2016-06-15 14:12:28 -0400

    Static string tests

--- Tests/API/String/StaticAndCStyle.cpp
+++ Tests/API/String/StaticAndCStyle.cpp
@@ -26,3 +26,36 @@ SCENARIO("CStyle strings work", "[API][String][CStyle][Unit]") {
 		}
 	}
 }
+
+SCENARIO("Static strings work", "[API][String][Static][Unit]") {
+	GIVEN("A valid Static string") {
+		const char* const TestData(u8"asdf");
+		WHEN("It is used to instantiate a Static") {
+			CYB::API::String::Static TestString(TestData);
+			THEN("The byte length is correct") {
+				CHECK(TestString.RawLength() == 4);
+			}
+			THEN("The length is correct") {
+				CHECK(TestString.Length() == 4);
+			}
+			THEN("The data matches") {
+				CHECK(std::strcmp(TestString.CString(), TestData) == 0);
+			}
+		}
+	}
+	GIVEN("Null string data") {
+		const char* const TestData(nullptr);
+		WHEN("It is used to instantiate a Static") {
+			CYB::API::String::Static TestString(TestData);
+			THEN("The byte length is correct") {
+				CHECK(TestString.RawLength() == 0);
+			}
+			THEN("The length is correct") {
+				CHECK(TestString.Length() == 0);
+			}
+			THEN("The data matches") {
+				CHECK(TestString.CString() == nullptr);
+			}
+		}
+	}
+}

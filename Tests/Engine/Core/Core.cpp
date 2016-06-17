063e4f492699f0a2aa77dccd07fc2955d5296dd0
Cyberboss <cyberboss1994@hotmail.com>
2016-06-17 15:58:41 -0400

    Fixed the core instantiation test not calling the destructor

--- Tests/Engine/Core/Core.cpp
+++ Tests/Engine/Core/Core.cpp
@@ -2,27 +2,47 @@
 
 //Heavy whiteboxing below
 
+struct CoreConstructor;
+
+template<> void CYB::Engine::Core::Backdoor<CoreConstructor>(CoreConstructor& AConstructor);
+
 struct CoreConstructor {
 	byte FBytes[sizeof(CYB::Engine::Core)];
 	const unsigned int FNumArguments;
 	const oschar_t* const* const FArguments;
+	bool FConstructing;
+
+	CoreConstructor(const unsigned int ANumArguments, const oschar_t* const* const AArguments) :
+		FNumArguments(ANumArguments),
+		FArguments(AArguments),
+		FConstructing(true)
+	{
+		CYB::Engine::Core::Backdoor(*this);
+	}
+	~CoreConstructor() {
+		FConstructing = false;
+		CYB::Engine::Core::Backdoor(*this);
+	}
 };
 
 template<> void CYB::Engine::Core::Backdoor<CoreConstructor>(CoreConstructor& AConstructor) {
-	new (static_cast<void*>(AConstructor.FBytes)) Core(AConstructor.FNumArguments, AConstructor.FArguments);
+	if (AConstructor.FConstructing)
+		new (static_cast<void*>(AConstructor.FBytes)) Core(AConstructor.FNumArguments, AConstructor.FArguments);
+	else
+		FSingleton->~Core();
 }
 
 SCENARIO("Core instantiation", "[Engine][Core][Functional]") {
 	Fake::Core::NullifySingleton();
 	GIVEN("The core") {
-		typedef CYB::Engine::Core TheCore;
+		CoreConstructor* Constructor(nullptr);
 		WHEN("It is instantiated") {
-			CoreConstructor Constructor{ {}, 0, nullptr };
-			TheCore::Backdoor(Constructor);
+			Constructor = new CoreConstructor(0, nullptr);
 			THEN("The engine is properly initialized"){
-				CHECK(static_cast<void*>(&CYB::Core()) == static_cast<void*>(Constructor.FBytes));
+				CHECK(static_cast<void*>(&CYB::Core()) == static_cast<void*>(Constructor->FBytes));
 			}
 		}
+		delete Constructor;
 	}
 	Fake::Core::ResetToFakeCorePointer();
 }

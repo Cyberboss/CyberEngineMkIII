a6635c72a48be3238c7b2ee8819160fb412602b7
Cyberboss <cyberboss1994@hotmail.com>
2016-06-17 15:45:31 -0400

    Switched to the unicode LoadLibrary API in the depths of the syscall.

--- Engine/Platform/System/Win32/CYBWin32Syscalls.cpp
+++ Engine/Platform/System/Win32/CYBWin32Syscalls.cpp
@@ -35,16 +35,25 @@ unsigned long long CYB::Platform::System::Sys::LinkedCall(const CallNumber ACall
 		Win32::FreeLibrary(static_cast<Win32::HMODULE>(AArg1.FPointer));
 		return 0;
 	case LOAD_LIBRARY:
-		return reinterpret_cast<unsigned long long>(Win32::LoadLibraryA(static_cast<const char*>(AArg1.FPointer)));
+	{
+		CYB::API::String::Static String(static_cast<const char* const>(AArg1.FPointer));
+		wchar_t Unicode[MAX_PATH];
+		API::Assert::LessThan(String.RawLength(), static_cast<unsigned int>(std::numeric_limits<int>::max()));
+		//+1 for the \0
+		if (Win32::MultiByteToWideChar(CP_UTF8, 0, String.CString(), static_cast<int>(String.RawLength() + 1), Unicode, MAX_PATH) > MAX_PATH)
+			return 0;
+		return reinterpret_cast<unsigned long long>(Win32::LoadLibraryW(Unicode));
+	}
 	case LOAD_SYMBOL:
 	default:
 		API::Assert::HCF();
 	}
 }
 unsigned long long CYB::Platform::System::Sys::LinkedCall(const CallNumber ACallNumber, const Union64 AArg1, const Union64 AArg2) noexcept {
+	using namespace Win32;
 	switch (ACallNumber) {
 	case LOAD_SYMBOL:
-		return reinterpret_cast<unsigned long long>(Win32::GetProcAddress(static_cast<Win32::HMODULE>(AArg1.FPointer), static_cast<const char* const>(AArg2.FPointer)));
+		return reinterpret_cast<unsigned long long>(GetProcAddress(static_cast<HMODULE>(AArg1.FPointer), static_cast<const char* const>(AArg2.FPointer)));
 	case TERMINATE_PROC:
 	case CLOSE_LIBRARY:
 	case LOAD_LIBRARY:

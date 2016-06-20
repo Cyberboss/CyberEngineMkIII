3279e34e12326906e40462f823e61418c17d8e2d
Cyberboss <cyberboss1994@hotmail.com>
2016-06-20 13:06:44 -0400

    Some strings stuff and Path API modification

---
+++ Engine/API/Types/String/UTF8String.hpp
@@ -0,0 +1,13 @@
+//! @file UTF8String.hpp Defines CYB::API::String::UTF8
+#pragma once
+
+namespace CYB {
+	namespace API {
+		namespace String {
+			//! @brief A variable length UTF-8 string
+			class UTF8 : public Dynamic {
+
+			};
+		};
+	};
+};

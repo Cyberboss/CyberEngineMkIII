8312c231404925a5243c76b3a9979ea2f91331df
Cyberboss <cyberboss1994@hotmail.com>
2016-06-22 09:52:26 -0400

    Fixed doc script

--- BuildScripts/Doxygen/TFSUploadDox.ps1
+++ BuildScripts/Doxygen/TFSUploadDox.ps1
@@ -8,4 +8,9 @@ rm -r "$TARGETDIR/*"
 cp -r Documentation/CyberEngineMkIII/* $TARGETDIR
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/API', 'CyberEngineMkIIIAPIDox.zip')
-[IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/Engine', 'CyberEngineMkIIIDox.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/WindowsDebugEngine', 'CyberEngineMkIIIWindowsDebugDox.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/WindowsEngine', 'CyberEngineMkIIIWindowsDox.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/OSXDebugEngine', 'CyberEngineMkIIIOSXDebugDox.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/OSXEngine', 'CyberEngineMkIIIOSXDox.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/LinuxDebugEngine', 'CyberEngineMkIIILinuxDebugDox.zip')
+[IO.Compression.ZipFile]::CreateFromDirectory('Documentation/CyberEngineMkIII/LinuxEngine', 'CyberEngineMkIIILinuxDox.zip')

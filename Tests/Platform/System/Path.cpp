4124e118bfa6bfbc405525d4ea180134cecffcd6
Cyberboss <Cyberboss@users.noreply.github.com>
2016-07-07 12:32:53 -0400

    More code coverage

--- Tests/Platform/System/Path.cpp
+++ Tests/Platform/System/Path.cpp
@@ -2,7 +2,7 @@
 
 using namespace CYB::Platform::System;
 
-SCENARIO("Paths can be created by the system", "[Platform][System][Path][Unit") {
+SCENARIO("Paths can be created by the system", "[Platform][System][Path][Unit]") {
 	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMKernel32> K32(CYB::Core().FModuleManager.FK32);
 	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShellAPI> ShellAPI(CYB::Core().FModuleManager.FShellAPI);
 	ModuleDependancy<CYB::API::Platform::POSIX, CYB::Platform::Modules::AMLibC> LibC(CYB::Core().FModuleManager.FC);
@@ -14,6 +14,49 @@ SCENARIO("Paths can be created by the system", "[Platform][System][Path][Unit") 
 				CHECK(TestPath != nullptr);
 			}
 			delete TestPath;
+		}
+	}
+}
+
+REDIRECTED_FUNCTION(BadPathRemoveFileSpec, void* const) {
+	return 0;
+}
+
+REDIRECTED_FUNCTION(BadGetModuleHandle, void* const) {
+	return static_cast<void*>(nullptr);
+}
+
+REDIRECTED_FUNCTION(BadReadLink, const void* const, const void* const, const long long) {
+	return 0;
+}
+
+SCENARIO("Path errors work", "[Platform][System][Path][Unit]") {
+	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMKernel32> K32(CYB::Core().FModuleManager.FK32);
+	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShellAPI> ShellAPI(CYB::Core().FModuleManager.FShellAPI);
+	ModuleDependancy<CYB::API::Platform::POSIX, CYB::Platform::Modules::AMLibC> LibC(CYB::Core().FModuleManager.FC);
+	GIVEN("Faulty system path retrival") {
+		WHEN("An invalid enum is used") {
+			REQUIRE_THROWS_AS(Path TestPath((Path::SystemPath)-1), CYB::Exception::Violation);
+			THEN("The correct error is thrown") {
+				CHECK_EXCEPTION_CODE(CYB::Exception::Violation::INVALID_ENUM);
+			}
+		}
+		{
+			const auto BRL(LibC.Redirect<CYB::Platform::Modules::LibC::readlink, BadReadLink>());
+			WHEN("The executable path is corrupted") {
+				const auto BPRFS(ShellAPI.Redirect<CYB::Platform::Modules::ShellAPI::PathRemoveFileSpecW, BadPathRemoveFileSpec>());
+				REQUIRE_THROWS_AS(Path TestPath(Path::SystemPath::EXECUTABLE), CYB::Exception::SystemData);
+				THEN("The correct error is thrown") {
+					CHECK_EXCEPTION_CODE(CYB::Exception::SystemData::SYSTEM_PATH_RETRIEVAL_FAILURE);
+				}
+			}
+			WHEN("The executable image path is corrupted") {
+				const auto BGMH(K32.Redirect<CYB::Platform::Modules::Kernel32::GetModuleHandleW, BadGetModuleHandle>());
+				REQUIRE_THROWS_AS(Path TestPath(Path::SystemPath::EXECUTABLE), CYB::Exception::SystemData);
+				THEN("The correct error is thrown") {
+					CHECK_EXCEPTION_CODE(CYB::Exception::SystemData::SYSTEM_PATH_RETRIEVAL_FAILURE);
+				}
+			}
 		}
 	}
 }

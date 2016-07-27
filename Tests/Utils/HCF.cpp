2bc943efba140e0e06b4f4fae789bcd978bbb208
Cyberboss <cyberboss1994@hotmail.com>
2016-07-27 14:36:22 -0400

    Fixed the unimpl noexcept spec and went back to using FAIL

--- Tests/Utils/HCF.cpp
+++ Tests/Utils/HCF.cpp
@@ -5,14 +5,6 @@ void CYB::API::Assert::HCF[[noreturn]](void) noexcept {
 	std::terminate();
 }
 
-struct unimplEx{
-	std::string FData;
-};
-
-CATCH_TRANSLATE_EXCEPTION(unimplEx& ex) {
-	return ex.FData;
-}
-
 void CYB::API::Assert::Unimplemented(const char* const AFunction, const char* const AFile, const unsigned int ALine) {
-	throw unimplEx{ std::string("Call to unimplemented function \"") + AFunction + "\" in file \"" + AFile + "\" at line: " + std::to_string(ALine) };
+	FAIL(std::string("Call to unimplemented function \"") + AFunction + "\" in file \"" + AFile + "\" at line: " + std::to_string(ALine));
 }

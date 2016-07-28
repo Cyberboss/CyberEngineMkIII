7540e7f5250e745da46424e8101e66dcd120082a
Cyberboss <Cyberboss@users.noreply.github.com>
2016-07-28 10:27:48 -0400

    Fixed potential UTF16 buffer problems

--- Engine/Platform/SystemHeaders/CYBUTF16String.hpp
+++ Engine/Platform/SystemHeaders/CYBUTF16String.hpp
@@ -9,13 +9,25 @@ namespace CYB {
 				/*!
 					@brief Construct a raw allocated UTF16 wide char array casted to a char array given a UTF8 string
 					@param AUTF8 The UTF8 string to construct from
+					@param[out] ALengthReference Contains the length of the allocated buffer - 1 upon successful exit
 					@return A allocated char pointer to a wide string
 					@par Thread Safety
 						This function requires no thread safety
 					@throws CYB::Exception::SystemData Error code: CYB::Exception::SystemData::HEAP_ALLOCATION_FAILURE. Thrown if the current heap runs out of memory
 					@throws CYB::Exception::Internal Error code: CYB::Exception::Internal::FAILED_TO_CONVERT_UTF16_STRING. Thrown if windows failed to convert the string
 				*/
-				static char* SetupData(const UTF8& AUTF8);
+				static char* SetupData(const UTF8& AUTF8, unsigned long long& ALengthReference);
+
+				/*!
+					@brief Construct a new UTF16 from a UTF8 string
+					@param AUTF8 The UTF8 string to construct from
+					@param ALengthReference Ignored and used internally
+					@par Thread Safety
+						This function requires no thread safety
+					@throws CYB::Exception::SystemData Error code: CYB::Exception::SystemData::HEAP_ALLOCATION_FAILURE. Thrown if the current heap runs out of memory
+					@throws CYB::Exception::Internal Error code: CYB::Exception::Internal::FAILED_TO_CONVERT_UTF16_STRING. Thrown if windows failed to convert the string
+				*/
+				UTF16(const UTF8& AUTF8, unsigned long long ALengthReference);
 			public:
 				/*!
 					@brief Create a new UTF8 string given a wide char array

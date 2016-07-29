fb6d19c809b97a7e9d35f984cad06dcb6ca445ef
Cyberboss <cyberboss1994@hotmail.com>
2016-07-29 11:06:53 -0400

    Constructor improvements

--- Engine/API/Interop/Constructor.hpp
+++ Engine/API/Interop/Constructor.hpp
@@ -6,10 +6,20 @@ namespace CYB {
 			/*!
 				@brief Template for defining the types of parameters for engine object constructors with multiple arguments
 				@tparam AType The last in the series of types
-			*/
+			*/																			//Doxygen too stupid for the recursive parse
 			template <typename AType, typename... ATypes> class Constructor : /*! @cond */ public Constructor<ATypes...> /*! @endcond */ {
 			public:
 				AType FParam;	//!< @brief The pointer to the parameter
+			protected:
+				/*!
+					@brief Construct a Constructor
+					@param AParam A pointer to a valid AType
+					@param AParams The remaining pointers
+					@param ASize The size of the most childish class
+					@par Thread Safety
+						This function requires no thread safety
+				*/
+				Constructor(AType&& AParam, ATypes&&... AParams, const unsigned short ASize) noexcept;
 			public:
 				/*!
 					@brief Construct a Constructor
@@ -18,13 +28,34 @@ namespace CYB {
 					@par Thread Safety
 						This function requires no thread safety
 				*/
-				Constructor(AType AParam, ATypes&&... AParams) noexcept :
-					Constructor<ATypes...>(std::forward(AParams)...),
-					FParam(AParam)
-				{}
+				Constructor(AType&& AParam, ATypes&&... AParams) noexcept;
 			};
 			//! @brief Used as a base class by all constructors
-			template <> class Constructor<void> {};
+			template <> class Constructor<void> {
+			private:
+				const unsigned short FSize;	//!< @brief The number of arguments in the constructor
+			protected:
+				/*!
+					@brief Construct a non-empty Constructor
+					@param ASize The size of the most childish class
+					@par Thread Safety
+						This function requires no thread safety
+				*/
+				Constructor(const unsigned short ASize) noexcept;
+			public:
+				/*!
+					@brief Construct an empty Constructor
+					@par Thread Safety
+						This function requires no thread safety
+				*/
+				Constructor() noexcept;
+				
+				/*!
+					@brief A best effort validation for the integrity of a Constructor for a given Allocatable
+					@return True if the Constructor is valid, false otherwize
+				*/
+				template <class AAllocatable> bool Valid(void) const noexcept;
+			};
 			/*!
 				@brief Template for defining the types of parameters for engine object constructors
 				@tparam AType The last in the series of types
@@ -32,6 +63,15 @@ namespace CYB {
 			template <typename AType> class Constructor<AType> : public Constructor<void> {
 			public:
 				AType FParam;	//!< @brief The pointer to the parameter
+			protected:
+				/*!
+					@brief Construct a Constructor
+					@param AParam A pointer to a valid AType
+					@param ASize The size of the most childish class
+					@par Thread Safety
+						This function requires no thread safety
+				*/
+				Constructor(AType&& AParam, const unsigned short ASize) noexcept;
 			public:
 				/*!
 					@brief Construct a Constructor
@@ -39,11 +79,11 @@ namespace CYB {
 					@par Thread Safety
 						This function requires no thread safety
 				*/
-				Constructor(AType AParam) noexcept :
-					FParam(AParam)
-				{}
+				Constructor(AType&& AParam) noexcept;
 			};
 			typedef Constructor<void> EmptyConstructor;
 		};
 	};
 };
+
+#include "Constructor.inl"

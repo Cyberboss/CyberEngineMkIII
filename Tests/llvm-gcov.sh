d5765b7ef1e74571b1f2df72e5457d1a6d1b6be4
Cyberboss <cyberboss1994@hotmail.com>
2016-08-02 11:34:13 -0400

    lcov now has block coverage, god help me

--- Tests/llvm-gcov.sh
+++ Tests/llvm-gcov.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$(uname)" == "Darwin" ]]; then
-	exec /Library/Developer/CommandLineTools/usr/bin/llvm-cov gcov "$@"
+	exec /Library/Developer/CommandLineTools/usr/bin/llvm-cov gcov -a "$@"
 else
-	exec llvm-cov gcov "$@"
+	exec llvm-cov gcov -a "$@"
 fi

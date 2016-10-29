667e76277a98f5d913b2a71f1a7b7d6d34f7b2e4
Cyberboss <cyberboss1994@hotmail.com>
2016-10-29 03:28:12 -0400

    Implement the rest of the dumb STLAllocator

--- Engine/API/Interop/STLAllocator.hpp
+++ Engine/API/Interop/STLAllocator.hpp
@@ -4,7 +4,7 @@ namespace CYB {
 	namespace API {
 		namespace Interop {
 			//! @brief Remaps STL style allocations to the CyberEngine Allocator. Based on std::allocator, usable on STL containers
-			template <class AType> class STLAllocator {
+			template <typename AType> class STLAllocator {
 			public:
 				using pointer = AType*;	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::pointer</a>
 				using const_pointer = const AType*;	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::const_pointer</a>
@@ -13,16 +13,26 @@ namespace CYB {
 				using size_type = unsigned long long;	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::size_type</a>
 				using difference_type = std::ptrdiff_t;	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::difference_type</a>
 
-				template<class AOther> struct rebind { typedef STLAllocator<AOther> other; };	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::rebind</a>
+				template<typename AOther> class rebind { public: typedef STLAllocator<AOther> other; };	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::rebind</a>
 
 				using value_type = AType;	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator</a>
 				using propagate_on_container_move_assignment = std::false_type;	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator</a>
 				using is_always_equal = std::true_type;	//!< @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator</a>
 			public:
+				//! @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::address</a>
+				static pointer address(reference AObject) noexcept;
+				//! @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator">std::allocator::address</a>
+				static const_pointer address(const_reference AObject) noexcept;
+
 				//! @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator/allocate">std::allocator::allocate</a>
 				static AType* allocate(const std::size_t ACount);
 				//! @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator/deallocate">std::allocator::deallocate</a>
 				static void deallocate(AType* const AObject, const std::size_t ACount) noexcept;
+
+				//! @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator/construct">std::allocator::construct</a>
+				template <typename AOther, typename... AArgs> static void construct(AOther* const APointer, AArgs&&... AArguments);
+				//! @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator/destroy">std::allocator::destroy</a>
+				template <typename AOther> static void destroy(AOther* const APointer) noexcept;
 
 				//! @brief See <a href="http://en.cppreference.com/w/cpp/memory/allocator/allocator">std::allocator::allocator</a>
 				STLAllocator() noexcept = default;
--- Engine/API/Interop/STLAllocator.inl
+++ Engine/API/Interop/STLAllocator.inl
@@ -1,27 +1,44 @@
 #pragma once
 
-template <class AType> AType* CYB::API::Interop::STLAllocator<AType>::allocate(const std::size_t ACount) {
+template <typename AType> auto CYB::API::Interop::STLAllocator<AType>::address(reference AObject) noexcept -> pointer {
+	return std::addressof<AType>(AObject);
+}
+
+template <typename AType> auto CYB::API::Interop::STLAllocator<AType>::address(const_reference AObject) noexcept -> const_pointer {
+	return std::addressof<AType>(AObject);
+}
+
+template <typename AType> AType* CYB::API::Interop::STLAllocator<AType>::allocate(const std::size_t ACount) {
 	const auto Size(sizeof(AType) * ACount);
 	Assert::LessThanOrEqual<unsigned long long>(Size, static_cast<unsigned long long>(std::numeric_limits<int>::max()));
 	return static_cast<AType*>(Context::GetContext().FAllocator.FHeap.Alloc(static_cast<int>(Size)));
 }
 
-template <class AType> void CYB::API::Interop::STLAllocator<AType>::deallocate(AType* const AObject, const std::size_t ACount) noexcept {
+template <typename AType> void CYB::API::Interop::STLAllocator<AType>::deallocate(AType* const AObject, const std::size_t ACount) noexcept {
 	const auto Size(sizeof(AType) * ACount);
 	Assert::LessThanOrEqual<unsigned long long>(Size, static_cast<unsigned long long>(std::numeric_limits<int>::max()));
 	Context::GetContext().FAllocator.FHeap.Free(AObject);
 }
 
-template <class AType> template <class AOtherType> CYB::API::Interop::STLAllocator<AType>::STLAllocator(const AOtherType& AOther) noexcept {
+template <typename AType> template <typename AOther, typename... AArgs> void CYB::API::Interop::STLAllocator<AType>::construct(AOther* const APointer, AArgs&&... AArguments) {
+	::new (static_cast<void*>(APointer)) AOther(std::forward<AArgs>(AArguments)...);
+}
+
+template <typename AType> template <typename AOther> void CYB::API::Interop::STLAllocator<AType>::destroy(AOther* const APointer) noexcept {
+	static_cast<void>(APointer);	//MSVC thinks calling the destructor counts as having the pointer being unreferenced
+	APointer->~AOther();
+}
+
+template <typename AType> template <typename AOtherType> CYB::API::Interop::STLAllocator<AType>::STLAllocator(const AOtherType& AOther) noexcept {
 	static_cast<void>(AOther);
 }
 
-template <class AType> template<class ARType> constexpr bool CYB::API::Interop::STLAllocator<AType>::operator==(const STLAllocator<ARType>& ARHS) const noexcept {
+template <typename AType> template<typename ARType> constexpr bool CYB::API::Interop::STLAllocator<AType>::operator==(const STLAllocator<ARType>& ARHS) const noexcept {
 	static_cast<void>(ARHS);
 	return true;
 }
 
-template <class AType> template<class ARType> constexpr bool CYB::API::Interop::STLAllocator<AType>::operator!=(const STLAllocator<ARType>& ARHS) const noexcept {
+template <typename AType> template<typename ARType> constexpr bool CYB::API::Interop::STLAllocator<AType>::operator!=(const STLAllocator<ARType>& ARHS) const noexcept {
 	static_cast<void>(ARHS);
 	return false;
 }

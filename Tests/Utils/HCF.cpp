e362e359c3994c24f9b862aa2fa36dde1719c815
Cyberboss <Cyberboss@users.noreply.github.com>
2016-10-29 03:59:02 -0400

    Modify the repo to work with Travis CI (#1)
    * Modified tree to use C++17 to fight STL allocator errors
    
    * Fixed syntax
    
    * Removed semicolon warning because old habits die hard
    
    * Removed pie and modules
    
    * Forgot that cotire requires it to be set at the target level
    
    * Forced to add rebind it seems like
    
    * Gotta learn to build locally
    
    * Debug attempt: checking the std version
    
    * Wow I can't see the glaring 14 right in front of me
    
    * Fine, I'll fix the bloody allocator for C++14 since 17 isn't ready yet.
    
    * Fixed doxygen comments
    
    * Implement the rest of the dumb STLAllocator
    
    * Forced to disable the gold plugin. Sad day indeed.
    
    * Removed the intentional failure on calls to unimplemented functions

--- Tests/Utils/HCF.cpp
+++ Tests/Utils/HCF.cpp
@@ -7,5 +7,4 @@ void CYB::API::Assert::HCF[[noreturn]](void) noexcept {
 
 void CYB::API::Assert::Unimplemented(const char* const AFunction, const char* const AFile, const unsigned int ALine) noexcept {
 	WARN(std::string("Call to unimplemented function \"") + AFunction + "\" in file \"" + AFile + "\" at line: " + std::to_string(ALine));
-	CHECK(false);
 }

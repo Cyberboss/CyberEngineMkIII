10d6bb66cf5809b47317dc032da834fe79327114
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 15:20:53 -0400

    Even snowflakier

--- BuildScripts/Appveyor/InstallCatchAdapter.ps1
+++ BuildScripts/Appveyor/InstallCatchAdapter.ps1
@@ -1,2 +1,1 @@
-$client = New-Object System.Net.WebClient
-$client.DownloadFile("https://github.com/CyberbossJHCB/VSCatchAdapter/releases/download/0.1.0/VSCatchAdapter.vsix", "VSCatchAdapter.vsix")
+wget https://github.com/CyberbossJHCB/VSCatchAdapter/releases/download/0.1.0/VSCatchAdapter.vsix

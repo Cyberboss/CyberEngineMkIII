fa6c96fc612b20e6b9c50e7c322ce8ff229beddd
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 16:50:51 -0400

    Do you want the actual dll?

--- BuildScripts/Appveyor/InstallCatchAdapter.ps1
+++ BuildScripts/Appveyor/InstallCatchAdapter.ps1
@@ -2,4 +2,3 @@ Write-Host "Downloading VSCatchAdapter v0.1.0..."
 appveyor DownloadFile  https://github.com/CyberbossJHCB/VSCatchAdapter/releases/download/0.1.0/Release.zip
 Write-Host "Unzipping..."
 &"7z.exe" e Release.zip
-ls

3e16c6c2babd2de7c485905e16726e942236b4c3
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 15:58:52 -0400

    Trying things a simpler way

--- BuildScripts/Appveyor/RunTests.ps1
+++ BuildScripts/Appveyor/RunTests.ps1
@@ -1,6 +1,2 @@
 # run tests
-&"vstest.console" /logger:Appveyor /UseVsixExtensions:true Output/bin/CyberEngineMkIIITester.exe /xml cyb-test-results.xml
 
-# upload results to AppVeyor
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path cyb-test-results.xml))

9ffa6046422c4f05541f7923d8d4d2a3f6cbf48b
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 13:32:23 -0400

    Use the default compiler on OSX

--- BuildScripts/Travis/Travis.sh
+++ BuildScripts/Travis/Travis.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+#override the clag version we set for linux
+if [ "$(uname)" == "Darwin" ]; then
+	$CXXCOMPILER=clang++
+fi
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	cmake . -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Release -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
 else

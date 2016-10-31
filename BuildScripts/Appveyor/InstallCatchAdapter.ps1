a2613f964a82e686fbeb7e234e11e58aac5d4e5f
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 16:57:49 -0400

    Are we using the right version?

--- BuildScripts/Appveyor/InstallCatchAdapter.ps1
+++ BuildScripts/Appveyor/InstallCatchAdapter.ps1
@@ -2,3 +2,5 @@ Write-Host "Downloading VSCatchAdapter v0.1.0..."
 appveyor DownloadFile  https://github.com/CyberbossJHCB/VSCatchAdapter/releases/download/0.1.0/Release.zip
 Write-Host "Unzipping..."
 &"7z.exe" e Release.zip
+
+&"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /logger:Appveyor /Platform:x64 /TestAdapterPath:"./" /inIsolation /Enablecodecoverage Output/bin/CyberEngineMkIIITester.exe

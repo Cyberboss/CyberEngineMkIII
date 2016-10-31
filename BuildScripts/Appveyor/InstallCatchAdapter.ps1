1831ec2933af7f60265ca1f34cd93924902ab406
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 16:27:22 -0400

    Let's just see if this'll work for now

--- BuildScripts/Appveyor/InstallCatchAdapter.ps1
+++ BuildScripts/Appveyor/InstallCatchAdapter.ps1
@@ -2,3 +2,5 @@ Write-Host "Downloading VSCatchAdapter v0.1.0..."
 wget https://github.com/CyberbossJHCB/VSCatchAdapter/releases/download/0.1.0/VSCatchAdapter.vsix
 Write-Host "Installing VSIX..."
 &"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" /q /a VSCatchAdapter.vsix
+
+&"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /logger:Appveyor /UseVsixExtensions:true /Platform:x64 /inIsolation /Enablecodecoverage Output/bin/CyberEngineMkIIITester.exe

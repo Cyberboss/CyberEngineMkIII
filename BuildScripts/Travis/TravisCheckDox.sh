cc1d71dc99a7a4acf71ce262d7d3aeb6c8a77317
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 09:15:46 -0400

    Yet even more script fixes

--- BuildScripts/Travis/TravisCheckDox.sh
+++ BuildScripts/Travis/TravisCheckDox.sh
@@ -2,24 +2,21 @@
 
 #Check all 3 platforms for dox errors
 mkdir TestDocs
-doxfile="$cat 'BuildScripts/Doxygen/CyberEngineMkIIIDoxy.cfg' ; echo 'EXPAND_AS_DEFINED = DEBUG TARGET_OS_WINDOWS' ; echo 'OUTPUT_DIRECTORY = TestDocs'; echo 'EXCLUDE_PATTERNS = CYBPosix* CYBLinux* CYBOSX*')"
-echo $doxfile | doxygen 2>&1 | tee doxygen.log
+(cat 'BuildScripts/Doxygen/CyberEngineMkIIIDoxy.cfg' ; echo 'EXPAND_AS_DEFINED = DEBUG TARGET_OS_WINDOWS' ; echo 'OUTPUT_DIRECTORY = TestDocs'; echo 'EXCLUDE_PATTERNS = CYBPosix* CYBLinux* CYBOSX*') | doxygen 2>&1 | tee doxygen.log
 
 if grep -q -i "warning" "doxygen.log"; then
 	echo 'Warnings detected in documentation. Failing build!'
 	exit 1
  fi
  
-doxfile="$(cat 'BuildScripts/Doxygen/CyberEngineMkIIIDoxy.cfg' ; echo 'EXPAND_AS_DEFINED = DEBUG TARGET_OS_MAC' ; echo 'OUTPUT_DIRECTORY = TestDocs'; echo 'EXCLUDE_PATTERNS = CYBWin32* CYBOSX*')"
-echo $doxfile | doxygen 2>&1 | tee doxygen.log
+(cat 'BuildScripts/Doxygen/CyberEngineMkIIIDoxy.cfg' ; echo 'EXPAND_AS_DEFINED = DEBUG TARGET_OS_MAC' ; echo 'OUTPUT_DIRECTORY = TestDocs'; echo 'EXCLUDE_PATTERNS = CYBWin32* CYBOSX*') | doxygen 2>&1 | tee doxygen.log
 
 if grep -q -i "warning" "doxygen.log"; then
 	echo 'Warnings detected in documentation. Failing build!'
 	exit 1
  fi
  
-doxfile="$cat 'BuildScripts/Doxygen/CyberEngineMkIIIDoxy.cfg' ; echo 'EXPAND_AS_DEFINED = DEBUG TARGET_OS_LINUX' ; echo 'OUTPUT_DIRECTORY = TestDocs'; echo 'EXCLUDE_PATTERNS = CYBLinux* CYBWin32*')"
-echo $doxfile | doxygen 2>&1 | tee doxygen.log
+(cat 'BuildScripts/Doxygen/CyberEngineMkIIIDoxy.cfg' ; echo 'EXPAND_AS_DEFINED = DEBUG TARGET_OS_LINUX' ; echo 'OUTPUT_DIRECTORY = TestDocs'; echo 'EXCLUDE_PATTERNS = CYBLinux* CYBWin32*') | doxygen 2>&1 | tee doxygen.log
 
 if grep -q -i "warning" "doxygen.log"; then
 	echo 'Warnings detected in documentation. Failing build!'

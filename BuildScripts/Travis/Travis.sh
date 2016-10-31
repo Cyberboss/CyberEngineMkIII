aa4dd23a8cbaa0bbe2f9742747c95698cc05bbd9
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 14:52:21 -0400

    Very not DRY but should get the job done

--- BuildScripts/Travis/Travis.sh
+++ BuildScripts/Travis/Travis.sh
@@ -2,16 +2,23 @@
 
 #override the clag version we set for linux
 if [ "$(uname)" == "Darwin" ]; then
-	export CXXCOMPILER=clang++
-	export CCOMPILER=clang
-fi
-
-if [ "${BUILD_RELEASE}" = "true" ]; then
-	echo 'Building release version'
-	cmake . -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Release -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
+	echo 'Detected OS: X'
+	if [ "${BUILD_RELEASE}" = "true" ]; then
+		echo 'Building release version'
+		cmake . -DCMAKE_BUILD_TYPE=Release -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1
+	else
+		echo 'Building debug version'
+		cmake . -DCMAKE_BUILD_TYPE=Debug -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1
+	fi
 else
-	echo 'Building debug version'
-	cmake . -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Debug -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
+	echo 'Detected OS: Linux'
+	if [ "${BUILD_RELEASE}" = "true" ]; then
+		echo 'Building release version'
+		cmake . -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Release -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
+	else
+		echo 'Building debug version'
+		cmake . -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Debug -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
+	fi
 fi
 make
 ctest

f8ab6514025c9fef9dfabf7bc2dcd5ae7f069908
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 16:21:39 -0400

    Try explicitly using the 14.0 test console

--- BuildScripts/Appveyor/InstallCatchAdapter.ps1
+++ BuildScripts/Appveyor/InstallCatchAdapter.ps1
@@ -1,4 +1,4 @@
 Write-Host "Downloading VSCatchAdapter v0.1.0..."
 wget https://github.com/CyberbossJHCB/VSCatchAdapter/releases/download/0.1.0/VSCatchAdapter.vsix
 Write-Host "Installing VSIX..."
-&"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" /a VSCatchAdapter.vsix
+&"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" /q /a VSCatchAdapter.vsix

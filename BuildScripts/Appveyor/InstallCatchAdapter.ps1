264b228dfe39f0d4f77e8d3b0499b68288062bfa
Cyberboss <cyberboss1994@hotmail.com>
2016-10-31 15:32:07 -0400

    And let's see if it all falls in place

--- BuildScripts/Appveyor/InstallCatchAdapter.ps1
+++ BuildScripts/Appveyor/InstallCatchAdapter.ps1
@@ -1,1 +1,2 @@
 wget https://github.com/CyberbossJHCB/VSCatchAdapter/releases/download/0.1.0/VSCatchAdapter.vsix
+&"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" /q /a VSCatchAdapter.vsix

e580f4d6649ae1f9b9675a7a9b02322964c3967a
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-04 14:47:06 -0400

    A better way of spamming module dependancies

--- Tests/Platform/System/File.cpp
+++ Tests/Platform/System/File.cpp
@@ -5,42 +5,67 @@ using namespace CYB::API::String;
 
 const char* const ALotOfData = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
+class NeededModules {
+public:
+	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMKernel32> FK32;
+	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShell> FShell;
+	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShellAPI> FShellAPI;
+	ModuleDependancy<CYB::API::Platform::POSIX, CYB::Platform::Modules::AMLibC> FC;
+public:
+	NeededModules() :
+		FK32(CYB::Core().FModuleManager.FK32),
+		FShell(CYB::Core().FModuleManager.FShell),
+		FShellAPI(CYB::Core().FModuleManager.FShellAPI),
+		FC(CYB::Core().FModuleManager.FC)
+	{}
+};
+
 SCENARIO("Files can be touched", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files sizes can be retrieved without opening them", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("File constructors work", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files sizes can be retrieved without opening them", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files can have their cursor position retrieved", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files can have their cursor position set set retrieved", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files can be read from", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files can be written to", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files' Paths can be retrieved", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }
 
 SCENARIO("Files' OpenMethod can be retrieved", "[Platform][System][File][Unit]") {
+	NeededModules TestModules;
 	FAIL("Unwritten test");
 }

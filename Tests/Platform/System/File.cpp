e98f6351308934d0f8912ead1fb2f255da1f336a
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-04 16:27:58 -0400

    Wrote the constructors tests

--- Tests/Platform/System/File.cpp
+++ Tests/Platform/System/File.cpp
@@ -13,6 +13,15 @@ public:
 	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShell> FShell;
 	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShellAPI> FShellAPI;
 	ModuleDependancy<CYB::API::Platform::POSIX, CYB::Platform::Modules::AMLibC> FC;
+private:
+	void Write(unsigned long long AAmount, Path&& AFile, const File::Method AMethod) {
+		File Data(std::move(AFile), File::Mode::WRITE, AMethod);
+		while (AAmount > 0) {
+			const auto DataWritten(Data.Write(ALotOfData, AAmount));
+			REQUIRE(DataWritten <= AAmount);
+			AAmount -= DataWritten;
+		}
+	}
 public:
 	TestStartup() :
 		FK32(CYB::Core().FModuleManager.FK32),
@@ -29,6 +38,18 @@ public:
 		REQUIRE_NOTHROW(TestPath2.reset(new Path(Path::SystemPath::TEMPORARY)));
 		REQUIRE_NOTHROW(TestPath2->Append(UTF8(Static("TestFile2")), false, false));
 	}
+	void Data1(const unsigned long long AAmount, const File::Method AMethod = File::Method::ANY) {
+		Write(AAmount, Path1(), AMethod);
+	}
+	void Data2(const unsigned long long AAmount, const File::Method AMethod = File::Method::ANY) {
+		Write(AAmount, Path2(), AMethod);
+	}
+	void Touch1(void) {
+		REQUIRE_NOTHROW(File::Touch(Path(*TestPath1)));
+	}
+	void Touch2(void) {
+		REQUIRE_NOTHROW(File::Touch(Path(*TestPath2)));
+	}
 	Path Path1() {
 		return *TestPath1;
 	}
@@ -42,7 +63,7 @@ SCENARIO("Files can be touched", "[Platform][System][File][Unit]") {
 	GIVEN("A path to touch") {
 		auto Path1(TestData.Path1());
 		WHEN("It is touched") {
-			File::Touch(Path(Path1));
+			CHECK_NOTHROW(File::Touch(Path(Path1)));
 			THEN("It now exists") {
 				CHECK(Path1.IsFile());
 			}
@@ -50,17 +71,123 @@ SCENARIO("Files can be touched", "[Platform][System][File][Unit]") {
 	}
 }
 
-SCENARIO("Files sizes can be retrieved without opening them", "[Platform][System][File][Unit]") {
-	TestStartup TestData;
-	FAIL("Unwritten test");
-}
-
 SCENARIO("File constructors work", "[Platform][System][File][Unit]") {
 	TestStartup TestData;
-	FAIL("Unwritten test");
+	File::Mode Mo;
+	File::Method Me;
+	Path* CurrentPath;
+	bool MovePath, Exists, Directory;
+
+	const auto Creation([&]() {
+		File(MovePath ? std::move(*CurrentPath) : *CurrentPath, Mo, Me);
+	});
+
+	const auto Then([&]() {
+		if(!Directory)
+			CHECK_NOTHROW(Creation());
+		else {
+			CHECK_THROWS_AS(Creation(), CYB::Exception::SystemData);
+			if(Mo == File::Mode::READ)
+				CHECK_EXCEPTION_CODE(CYB::Exception::SystemData::FILE_NOT_READABLE);
+			else
+				CHECK_EXCEPTION_CODE(CYB::Exception::SystemData::FILE_NOT_WRITABLE);
+		}
+	});
+	const auto MethodMatrix([&]() {
+		WHEN("We open with method ANY") {
+			Me = File::Method::ANY;
+			Then();
+		}
+		WHEN("We open with method CREATE") {
+			Me = File::Method::CREATE;
+			if(!Exists)
+				Then();
+			else {
+				THEN("It fails correctly") {
+					CHECK_THROWS_AS(File(MovePath ? std::move(*CurrentPath) : *CurrentPath, Mo, Me), CYB::Exception::SystemData);
+					CHECK_EXCEPTION_CODE(CYB::Exception::SystemData::FILE_EXISTS);
+				}
+			}
+		}
+		WHEN("We open with method TRUNCATE") {
+			Me = File::Method::TRUNCATE;
+			Then();
+		}
+		WHEN("We open with method EXIST") {
+			Me = File::Method::EXIST;
+			if (Exists)
+				Then();
+			else {
+				THEN("It fails correctly") {
+					CHECK_THROWS_AS(File(MovePath ? std::move(*CurrentPath) : *CurrentPath, Mo, Me), CYB::Exception::SystemData);
+					CHECK_EXCEPTION_CODE(CYB::Exception::SystemData::FILE_NOT_FOUND);
+				}
+			}
+		}
+	});
+
+	const auto ModeMatrix([&]() {
+		WHEN("We open with mode READ") {
+			Mo = File::Mode::READ;
+			MethodMatrix();
+		}
+		WHEN("We open with mode WRITE") {
+			Mo = File::Mode::WRITE;
+			MethodMatrix();
+		}
+		WHEN("We open with mode READ_WRITE") {
+			Mo = File::Mode::READ_WRITE;
+			MethodMatrix();
+		}
+	});
+	const auto ConstructorMatrix([&]() {
+		WHEN("We use the path move constructor") {
+			MovePath = true;
+			ModeMatrix();
+		}
+		WHEN("We use the path reference constructor") {
+			MovePath = false;
+			ModeMatrix();
+		}
+	});
+
+	GIVEN("A non-existant path") {
+		auto Test(TestData.Path1());
+		CurrentPath = &Test;
+		Exists = false;
+		Directory = false;
+		ConstructorMatrix();
+	}
+	GIVEN("A path that is a file") {
+		auto Test(TestData.Path1());
+		REQUIRE_NOTHROW(File::Touch(Path(Test)));
+		CurrentPath = &Test;
+		Exists = true;
+		Directory = false;
+		ConstructorMatrix();
+	}
+	GIVEN("A path that is a directory") {
+		auto Test(TestData.Path1());
+		REQUIRE_NOTHROW(Test.Append(UTF8(Static("SomeDir")), true, false));
+		REQUIRE_NOTHROW(File::Touch(Path(Test)));
+		CurrentPath = &Test;
+		Exists = true;
+		Directory = true;
+		ConstructorMatrix();
+	}
+	GIVEN("A path that is deleted") {
+		auto Test(TestData.Path1());
+		REQUIRE_NOTHROW(Test.Append(UTF8(Static("SomeDir")), true, false));
+		REQUIRE_NOTHROW(Test.Delete(true));
+		REQUIRE_NOTHROW(File::Touch(Path(Test)));
+		CurrentPath = &Test;
+		Exists = true;
+		Directory = false;
+		ConstructorMatrix();
+	}
 }
 
-SCENARIO("Files sizes can be retrieved without opening them", "[Platform][System][File][Unit]") {
+SCENARIO("Files sizes can be retrieved after opening them", "[Platform][System][File][Unit]") {
 	TestStartup TestData;
 	FAIL("Unwritten test");
 }
@@ -83,6 +210,20 @@ SCENARIO("Files can be read from", "[Platform][System][File][Unit]") {
 SCENARIO("Files can be written to", "[Platform][System][File][Unit]") {
 	TestStartup TestData;
 	FAIL("Unwritten test");
+}
+
+SCENARIO("Files sizes can be retrieved without opening them", "[Platform][System][File][Unit]") {
+	TestStartup TestData;
+	GIVEN("A touched file") {
+		TestData.Touch1();
+		WHEN("It's size is checked") {
+			unsigned long long Size(1);
+			CHECK_NOTHROW(Size = File::Size(TestData.Path1()));
+			THEN("It is zero") {
+				CHECK(Size == 0U);
+			}
+		}
+	}
 }
 
 SCENARIO("Files' Paths can be retrieved", "[Platform][System][File][Unit]") {

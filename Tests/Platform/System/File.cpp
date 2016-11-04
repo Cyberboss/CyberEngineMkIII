bccaecc60ecce289eacae2c6cc9de611b4367e27
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-04 14:32:05 -0400

    Frame the File tests

--- Tests/Platform/System/File.cpp
+++ Tests/Platform/System/File.cpp
@@ -3,25 +3,44 @@
 using namespace CYB::Platform::System;
 using namespace CYB::API::String;
 
-SCENARIO("File touching the unimplemented functions for code coverage", "[Platform][System][File][Unit]") {
-	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMKernel32> K32(CYB::Core().FModuleManager.FK32);
-	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShell> Shell(CYB::Core().FModuleManager.FShell);
-	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMOle32> OLE(CYB::Core().FModuleManager.FOLE);
-	ModuleDependancy<CYB::API::Platform::Identifier::WINDOWS, CYB::Platform::Modules::AMShellAPI> ShellAPI(CYB::Core().FModuleManager.FShellAPI);
-	ModuleDependancy<CYB::API::Platform::POSIX, CYB::Platform::Modules::AMLibC> LibC(CYB::Core().FModuleManager.FC);
-	ModuleDependancy<CYB::API::Platform::OSX, CYB::Platform::Modules::AMDyLD> DyLD(CYB::Core().FModuleManager.FDyLD);
+const char* const ALotOfData = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
-	Path Test(Path::SystemPath::TEMPORARY);
-	Test.Append(UTF8(Static("HelloWorld")), false, false);
-	File From(Test, File::Mode::READ_WRITE, File::Method::ANY);
-	File To(std::move(From));
-	Path Test2(Path::SystemPath::TEMPORARY);
-	Test2.Append(UTF8(Static("HelloWorld2")), false, false);
-	File TheFile(std::move(Test2), File::Mode::READ_WRITE, File::Method::ANY);
-	TheFile.Size();
-	TheFile.CursorPosition();
-	TheFile.Seek(0, File::SeekLocation::CURSOR);
-	TheFile.Read(nullptr, 0);
-	TheFile.Write(nullptr, 0);
-	TheFile.Path();
+SCENARIO("Files can be touched", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files sizes can be retrieved without opening them", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("File constructors work", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files sizes can be retrieved without opening them", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files can have their cursor position retrieved", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files can have their cursor position set set retrieved", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files can be read from", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files can be written to", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files' Paths can be retrieved", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
+}
+
+SCENARIO("Files' OpenMethod can be retrieved", "[Platform][System][File][Unit]") {
+	FAIL("Unwritten test");
 }

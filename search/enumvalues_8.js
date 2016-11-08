4ff1f8d9087e1105e4af0a06527fe90b850181e0
Travis CI <travis@travis-ci.org>
2016-11-08 20:32:16 +0000

    Deploy code docs to GitHub Pages Travis build: 184
    Commit: 9f46ef65dd94044e457eb7ecc746bf5c17bd04ff

--- search/enumvalues_8.js
+++ search/enumvalues_8.js
@@ -3,5 +3,6 @@ var searchData=
   ['idle',['IDLE',['../class_c_y_b_1_1_platform_1_1_system_1_1_thread.html#ad04681de3b2365896838cae4de12b69daa5daf7f2ebbba4975d61dab1c40188c7',1,'CYB::Platform::System::Thread']]],
   ['internal',['INTERNAL',['../class_c_y_b_1_1_exception_1_1_base.html#a7574334bdd01a9e3b98d1e7b0a6d8deba182fa1c42a2468f8488e6dcf75a81b81',1,'CYB::Exception::Base']]],
   ['invalid_5fenum',['INVALID_ENUM',['../class_c_y_b_1_1_exception_1_1_violation.html#a5e9d131ab13c9b938b708760974a0cb6ab2084d5d7652947b26aec1c7e239ca08',1,'CYB::Exception::Violation']]],
-  ['invalid_5finterop_5fconstructor',['INVALID_INTEROP_CONSTRUCTOR',['../class_c_y_b_1_1_exception_1_1_violation.html#a5e9d131ab13c9b938b708760974a0cb6aeab154d151b5700d865679cc9467d2ff',1,'CYB::Exception::Violation']]]
+  ['invalid_5finterop_5fconstructor',['INVALID_INTEROP_CONSTRUCTOR',['../class_c_y_b_1_1_exception_1_1_violation.html#a5e9d131ab13c9b938b708760974a0cb6aeab154d151b5700d865679cc9467d2ff',1,'CYB::Exception::Violation']]],
+  ['invalid_5fparameters',['INVALID_PARAMETERS',['../class_c_y_b_1_1_exception_1_1_violation.html#a5e9d131ab13c9b938b708760974a0cb6a1c6357cb0000f2a8f2e80e5454e23e5f',1,'CYB::Exception::Violation']]]
 ];

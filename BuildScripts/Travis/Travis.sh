699eb843f3b9321df1cb51b357ed6f406e519464
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-11 16:56:39 -0500

    Try again with coveralls now

--- BuildScripts/Travis/Travis.sh
+++ BuildScripts/Travis/Travis.sh
@@ -20,8 +20,4 @@ else
 		cmake . -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Debug -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
 	fi
 fi
-
-#setup coverage
-lcov --zerocounters --directory . --base-directory . --gcov-tool Tests/llvm-gcov.sh --rc
-
-scan-build -o StaticAnalysis make && ctest -T Test --output-on-failure
+cmake --build . && ctest --output-on-failure

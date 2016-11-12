f18613dccccf1bd7f709c90fc2c4d27e2e4976c6
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-11 20:36:33 -0500

    Hopefully this finishes the job

--- BuildScripts/Appveyor/UploadCoveralls.ps1
+++ BuildScripts/Appveyor/UploadCoveralls.ps1
@@ -1,16 +1,12 @@
 if ($env:CONFIGURATION -eq "Debug"){
-	ls
-	$test = ls
-	Write-Host $test
 	$coverageFilePath = Resolve-Path -path "TestResults\*\*.coverage"
-	$coveralls = Resolve-Path -path "*\tools\csmacnz.coveralls.exe.coverage"
  
 	$coverageFilePath = $coverageFilePath.ToString()
-	$coveralls = $coveralls.ToString()
  
 	if(Test-Path .\coverage.coveragexml){ rm .\coverage.coveragexml }
  
 	& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
  
+	$coveralls = "coveralls.net/tools/csmacnz.coveralls.exe"
 	& $coveralls --dynamiccodecoverage -i coverage.coveragexml --useRelativePaths
 }

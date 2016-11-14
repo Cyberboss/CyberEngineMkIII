6bc2cb97146bc70e18b1dc2bf1d509c5e05f0417
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-14 14:45:43 -0500

    Trying the python uploader

--- BuildScripts/Appveyor/GenCodeCovXML.ps1
+++ BuildScripts/Appveyor/GenCodeCovXML.ps1
@@ -6,6 +6,8 @@ if ($env:CONFIGURATION -eq "Debug"){
 	Write-Host "Running CodeCoverage.exe..."
 	&"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
 
+	rm -r TestResults
+
 	Write-Host "Downloading PathCapitalizationCorrector v0.1.4..."
 	appveyor DownloadFile https://github.com/Cyberboss/PathCapitalizationCorrector/releases/download/0.1.4/PathCapitalizationCorrector.exe
 

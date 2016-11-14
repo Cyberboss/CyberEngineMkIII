2d3d82e7d4859ce2e440f2153572904a8e6a5077
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-13 23:05:00 -0500

    Attempting a switch to CodeCov

---
+++ BuildScripts/Appveyor/GenCodeCovXML.ps1
@@ -0,0 +1,14 @@
+if ($env:CONFIGURATION -eq "Debug"){
+	$coverageFilePath = Resolve-Path -path "TestResults\*\*.coverage"
+ 
+	$coverageFilePath = $coverageFilePath.ToString()
+
+	Write-Host "Running CodeCoverage.exe..."
+	&"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
+
+	Write-Host "Downloading PathCapitalizationCorrector v0.1.4..."
+	appveyor DownloadFile https://github.com/Cyberboss/PathCapitalizationCorrector/releases/download/0.1.4/PathCapitalizationCorrector.exe
+
+	Write-Host "Fixing Window's terrible case ignorance..."
+	&"./PathCapitalizationCorrector.exe" coverage.coveragexml
+}

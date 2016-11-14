e15f8d2a29fb31707ff8ad67bad406d40e5a71ed
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-13 20:04:50 -0500

    Env variable had a space in it in appveyor, let's try again

--- BuildScripts/Appveyor/UploadCoveralls.ps1
+++ BuildScripts/Appveyor/UploadCoveralls.ps1
@@ -2,9 +2,7 @@ if ($env:CONFIGURATION -eq "Debug"){
 	$coverageFilePath = Resolve-Path -path "TestResults\*\*.coverage"
  
 	$coverageFilePath = $coverageFilePath.ToString()
- 
-	if(Test-Path .\coverage.coveragexml){ rm .\coverage.coveragexml }
- 
+
 	& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
  
 	$coveralls = "coveralls.net/tools/csmacnz.coveralls.exe"

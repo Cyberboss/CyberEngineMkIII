3765e9cb5d7d478f03004759f4febddad1c66fe3
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-16 11:41:27 -0500

    Get our own lcov with blackjack and hookers

--- BuildScripts/Coverage/run-lcov.sh
+++ BuildScripts/Coverage/run-lcov.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #run from project root
+curl -O -L https://github.com/linux-test-project/lcov/archive/v1.12.tar.gz
+gunzip v1.12.tar.gz
+tar -xvf v1.12.tar
+cd lcov-1.12
+make install
+cd ..
+rm -r v1.12.tar v1.12.tar.gz lcov-1.12
+
 lcov -v
+
 lcov --directory . --base-directory . --gcov-tool BuildScripts/Coverage/llvm-gcov.sh --no-external --capture -o lcovrun.dat --rc lcov_branch_coverage=1
 
 lcov --directory . --base-directory . -r lcovrun.dat *Assert.inl *Syscalls.* *.hpp -o lcovstripped.dat --rc lcov_branch_coverage=1 --gcov-tool BuildScripts/Coverage/llvm-gcov.sh 

34bb77aa987cd5e3d12f63aa9d50e49cad2fd1ba
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-17 15:29:48 -0500

    Let's do doxy a favor and match the weirdass signature they are giving us

--- Engine/Platform/System/CYBFile.hpp
+++ Engine/Platform/System/CYBFile.hpp
@@ -44,7 +44,7 @@ namespace CYB {
 					@throws CYB::Exception::Violation Error code: CYB::Exception::Violation::INVALID_ENUM. Thrown if @p AMode, or @p AMethod is invalid
 					@throws CYB::Exception::Violation Error code: CYB::Exception::Violation::INVALID_PARAMETERS. Thrown if @p AMode is Mode::READ and @p AMethod is Method::TRUNCATE. Operating systems require write permissions to truncate a file
 				*/
-				File(Path&& APath, const Mode AMode, const Method AMethod);
+				File(CYB::Platform::System::Path&& APath, const CYB::API::File::Mode AMode, const Method AMethod);
 				/*!
 					@brief Opens a file for access
 					@param APath The Path of the File to open

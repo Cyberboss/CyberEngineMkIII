00e97fc205e127a70ce2f834c5da83fecca6c0a8
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-24 11:49:56 -0500

    Fixes .codecov.yml and some coverage (#36)
    * Fixes .codecov.yml bugs
    
    * Some extra settings
    
    * Attempts to fix shitty OSS coverage. No offense
    
    * Dirty dirty fixes
    
    * We will keep changes as a required check
    
    But only for pulls please

--- Engine/Platform/Modules/Posix/CYBPosixModule.cpp
+++ Engine/Platform/Modules/Posix/CYBPosixModule.cpp
@@ -5,21 +5,22 @@ using namespace CYB::Platform::Posix;
 
 #include <cstring>
 
-CYB::Platform::Modules::Implementation::Module::Module(const API::String::CStyle& AModuleName) {
-	//This can be called without Core
-	FModule = reinterpret_cast<void*>(Platform::System::Sys::Call(Platform::System::Sys::LOAD_LIBRARY, const_cast<char*>(AModuleName.CString()), RTLD_LAZY));
+CYB::Platform::Modules::Implementation::Module::Module(const API::String::CStyle& AModuleName) 
+{	FModule = reinterpret_cast<void*>(Platform::System::Sys::Call(Platform::System::Sys::LOAD_LIBRARY, const_cast<char*>(AModuleName.CString()), RTLD_LAZY));	//This can be called without Core
 	if (FModule == nullptr)
 		throw Exception::Internal(Exception::Internal::MODULE_LOAD_FAILURE);
 }
-CYB::Platform::Modules::Implementation::Module::~Module() {
-	if (FModule != nullptr)
+CYB::Platform::Modules::Implementation::Module::~Module() 
+{	if (FModule != nullptr)
 		//This can be called without Core
 		Platform::System::Sys::Call(Platform::System::Sys::CLOSE_LIBRARY, FModule);
 }
 
-void* CYB::Platform::Modules::Module::LoadFunction(const API::String::CStyle& AFunctionName) {
+void* CYB::Platform::Modules::Module::LoadFunction(const API::String::CStyle& AFunctionName) 
+{
 	//This can be called without Core
 	auto Result(reinterpret_cast<void*>(Platform::System::Sys::Call(Platform::System::Sys::LOAD_SYMBOL, FModule, const_cast<char*>(AFunctionName.CString()))));
 	if (Result == nullptr)
 		throw Exception::Internal(Exception::Internal::MODULE_FUNCTION_LOAD_FAILURE);
-	return Result; }
+	return Result; 
+}

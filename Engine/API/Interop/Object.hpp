2fae910ca00036b1d09995700066cb25e1f77aca
Cyberboss <Cyberboss@users.noreply.github.com>
2016-12-19 11:19:57 -0500

    Removed the Allocatable limitation on Objects

--- Engine/API/Interop/Object.hpp
+++ Engine/API/Interop/Object.hpp
@@ -8,7 +8,6 @@ namespace CYB {
 				@tparam AInterface The interface to wrap
 			*/
 			template <class AInterface> class Object {
-				static_assert(std::is_base_of<Allocatable, AInterface>::value, "Objects can only be used with engine allocatables");
 			private:
 				AInterface* FPointer;	//!< @brief The pointer to the managed object
 			public:

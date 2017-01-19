33eef56e95d311a092b4d7990456b6a25e9f914c
Travis CI <travis@travis-ci.org>
2017-01-19 16:59:17 +0000

    Deploy code docs to GitHub Pages Travis build: 435
    Commit: 24015aa58294e2764a2fecd1060e984d4a4ec092

--- search/defines_3.js
+++ search/defines_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['unimplemented',['UNIMPLEMENTED',['../_assert_8hpp.html#a8608fe736d309b01e7bf6c4d414e3bf4',1,'Assert.hpp']]]
+  ['unimplemented',['UNIMPLEMENTED',['../_assert_8hpp.html#a8608fe736d309b01e7bf6c4d414e3bf4',1,'Assert.hpp']]],
+  ['unreachable',['UNREACHABLE',['../_assert_8hpp.html#a3af99897b60119951af8961dbd52dd3f',1,'UNREACHABLE():&#160;Assert.hpp'],['../_assert_8hpp.html#a3af99897b60119951af8961dbd52dd3f',1,'UNREACHABLE():&#160;Assert.hpp']]]
 ];

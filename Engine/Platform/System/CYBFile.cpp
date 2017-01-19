307944e212bd0cfe2f6cbb48172d637c7e70ed3f
Cyberboss <Cyberboss@users.noreply.github.com>
2017-01-19 14:16:26 -0500

    Revert "Coverage in File"
    This reverts commit 59ade51edecb89dcb9b50088959ad479daa2f182.

--- Engine/Platform/System/CYBFile.cpp
+++ Engine/Platform/System/CYBFile.cpp
@@ -1,42 +1,42 @@
 //! @file CYBFile.cpp Implements CYB::Platform::System::File shared functions
 #include "CYB.hpp"
 
-CYB::Platform::System::File::File(const API::Path& APath, const Mode AMode, Method AMethod) :
+CYB::Platform::System::File::File(const API::Path& APath, const Mode AMode, const Method AMethod) :
 	File(System::Path(static_cast<const System::Path&>(APath)), AMode, AMethod)
 {
+#ifdef DEBUG
 	using namespace API::String;
 	Dynamic Message(u8"Opened file: ");
 	Message += APath();
 	Message += Static(" in mode: ");
-
-	//weird ifs because coverage and we can guarantee no default case
-	if (AMode == Mode::READ) 
+	switch (AMode) {
+	case Mode::READ:
 		Message += Static("READ");
-	else if (AMode == Mode::WRITE)
+		break;
+	case Mode::WRITE:
 		Message += Static("WRITE");
-	else{
-		API::Assert::Equal(AMode, Mode::READ_WRITE);
+		break;
+	case Mode::READ_WRITE:
 		Message += Static("READ_WRITE");
-	}
-
+		break;
+	}	//default handled by base
 	Message += Static(" with method: ");
-
-	if (AMethod == Method::ANY) {
-		Message += Static("ANY to ");
-		AMethod = FOpenMethod;
-	}
-
-	if (AMethod == Method::CREATE)
+	switch (AMethod) {
+	case Method::ANY:
+		Message += Static("ANY");
+		break;
+	case Method::CREATE:
 		Message += Static("CREATE");
-	else if (AMethod == Method::EXIST)
+		break;
+	case Method::EXIST:
 		Message += Static("EXIST");
-	else {
-		API::Assert::NotEqual(AMethod, Method::ANY);
-		if (AMethod == Method::TRUNCATE)
-			Message += Static("TRUNCATE");
+		break;
+	case Method::TRUNCATE:
+		Message += Static("TRUNCATE");
+		break;
 	}
-
 	API::Context().FLogger.Log(Message, API::Logger::Level::DEV);
+#endif
 }
 
 void CYB::Platform::System::File::Touch(System::Path&& APath) {

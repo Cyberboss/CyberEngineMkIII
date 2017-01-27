f9f82e670463e1271d70b70b813be1f68e121163
Cyberboss <Cyberboss@users.noreply.github.com>
2017-01-27 09:45:45 -0500

    Switch to verbose Ctesting

--- BuildScripts/Travis/Travis.sh
+++ BuildScripts/Travis/Travis.sh
@@ -20,4 +20,4 @@ else
 		cmake . -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Debug -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
 	fi
 fi
-cmake --build . && ctest --output-on-failure
+cmake --build . && ctest -V

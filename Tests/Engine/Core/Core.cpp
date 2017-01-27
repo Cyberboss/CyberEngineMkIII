50aa6cf98d6e6916661a7017712efe0678a0ae65
Cyberboss <Cyberboss@users.noreply.github.com>
2017-01-27 15:02:19 -0500

    Revert "Debugging the OSX error"
    This reverts commit 73220d7b905b4c502cd97ab86dc09b074b59fe68.

--- Tests/Engine/Core/Core.cpp
+++ Tests/Engine/Core/Core.cpp
@@ -1,7 +1,6 @@
 #include "TestHeader.hpp"
 
 #include <cstring>
-#include <iostream>
 
 static bool FFakeProcExitRan;
 static unsigned long long FakeProcExit(Fake::SysCalls::Args&) {
@@ -32,24 +31,11 @@ static unsigned long long FakeLoadSym(Fake::SysCalls::Args& AArgs) {
 }
 
 SCENARIO("Core engine dry run", "[Engine][Core][Behavioural]") {
-	INFO("Starting test");
-	std::cout.flush();
 	GIVEN("A clean execution environment (With a redirected termination call of course)") {
-		std::cout.flush();
-		INFO("Here 1");
-		std::cout.flush();
 		FFakeProcExitRan = false;
-		INFO("Here 2");
-		std::cout.flush();
 		ModuleDependancy<CYB::Platform::Modules::Kernel32> K32;
-		INFO("Here 3");
-		std::cout.flush();
 		ModuleDependancy<CYB::Platform::Modules::LibC> LibC;
-		INFO("Here 4");
-		std::cout.flush();
 		Fake::Core::NullifySingleton();
-		INFO("Here 5");
-		std::cout.flush();
 		SysCallOverride Exit(
 #ifdef TARGET_OS_WINDOWS
 			CYB::Platform::System::Sys::TERMINATE_PROC
@@ -57,47 +43,23 @@ SCENARIO("Core engine dry run", "[Engine][Core][Behavioural]") {
 			CYB::Platform::System::Sys::EXIT_PROC
 #endif
 			, FakeProcExit);
-		INFO("Here 6");
-		std::cout.flush();
 		WHEN("The engine is run") {
-			std::cout.flush();
-			INFO("Here 7");
-			std::cout.flush();
 			CYB::Engine::Core::Run(0, nullptr);
-			INFO("Here 8");
-			std::cout.flush();
 			THEN("Certain things happened") {
 				CHECK(FFakeProcExitRan);
 			}
 		}
 		WHEN("A fake VM::Reserve pointer is set up") {
-			std::cout.flush();
-			INFO("Here 9");
-			std::cout.flush();
 			//what we'll do here is hook the syscall for VM::Reserve and return our own
 			SysCallOverride GetProc(CYB::Platform::System::Sys::LOAD_SYMBOL, FakeLoadSym);
-			INFO("Here 10");
-			std::cout.flush();
 			AND_THEN("The engine is run") {
-				std::cout.flush();
-				INFO("Here 11");
-				std::cout.flush();
 				CYB::Engine::Core::Run(0, nullptr);
-				INFO("Here 12");
-				std::cout.flush();
 				THEN("Certain things happened") {
-					std::cout.flush();
-					INFO("Here 13");
-					std::cout.flush();
 					CHECK(FFakeProcExitRan);
 					CHECK_EXCEPTION_CODE(CYB::Exception::Internal::MEMORY_RESERVATION_FAILURE);
 				}
 			}
 		}
-		INFO("Here 14");
-		std::cout.flush();
 		Fake::Core::ResetToFakeCorePointer();
-		INFO("Here 15");
-		std::cout.flush();
 	}
 }

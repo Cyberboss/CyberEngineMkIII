7f4d0b054465de3cb9d492cbb2a503d83c88ecef
Cyberboss <Cyberboss@users.noreply.github.com>
2017-01-27 11:25:42 -0500

    Go the extra mile for memory ordering in mutexes

--- Engine/Platform/System/Posix/CYBPosixMutex.cpp
+++ Engine/Platform/System/Posix/CYBPosixMutex.cpp
@@ -19,12 +19,16 @@ CYB::Platform::System::Mutex::~Mutex() {
 
 void CYB::Platform::System::Mutex::Lock(void) noexcept {
 	Core().FModuleManager.Call<Modules::PThread::pthread_mutex_lock>(&FMutex);
+	std::atomic_thread_fence(std::memory_order_acquire);
 }
 
 bool CYB::Platform::System::Mutex::TryLock(void) noexcept {
-	return Core().FModuleManager.Call<Modules::PThread::pthread_mutex_trylock>(&FMutex) == 0;
+	const auto Result(Core().FModuleManager.Call<Modules::PThread::pthread_mutex_trylock>(&FMutex) == 0);
+	std::atomic_thread_fence(std::memory_order_acquire);
+	return Result;
 }
 
 void CYB::Platform::System::Mutex::Unlock(void) noexcept {
+	std::atomic_thread_fence(std::memory_order_release);
 	Core().FModuleManager.Call<Modules::PThread::pthread_mutex_unlock>(&FMutex);
 }

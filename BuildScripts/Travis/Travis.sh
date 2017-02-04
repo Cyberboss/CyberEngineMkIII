9efc81d70135d8bf95e5dc416ad72b49770acbaa
Cyberboss <Cyberboss@users.noreply.github.com>
2017-02-03 20:31:37 -0500

    Fixes the build illegitimetely passing

--- BuildScripts/Travis/Travis.sh
+++ BuildScripts/Travis/Travis.sh
@@ -20,6 +20,4 @@ else
 		cmake . -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXOMPILER -DCMAKE_BUILD_TYPE=Debug -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=1 -DGOLD_PLUGIN=OFF
 	fi
 fi
-cmake --build . && ctest -V
-#cat the log files
-cat /tmp/CyberEngineMkIII/*txt 
+cmake --build . && ctest -V && cat /tmp/CyberEngineMkIII/*txt 

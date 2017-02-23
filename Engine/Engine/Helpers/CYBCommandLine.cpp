f3652d1d6ae58842afb300ed76d7b6d7108d2138
Cyberboss <Cyberboss@users.noreply.github.com>
2017-02-23 10:32:21 -0500

    Skeleton token parser

--- Engine/Engine/Helpers/CYBCommandLine.cpp
+++ Engine/Engine/Helpers/CYBCommandLine.cpp
@@ -1,7 +1,23 @@
 //! @file CYBCommandLine.cpp Implements CYB::Engine::Helpers::CommandLine
 #include "CYB.hpp"
 
-CYB::Engine::Helpers::CommandLine::CommandLine(const unsigned int ANumArguments, const oschar_t* const* const AArguments) {
-	static_cast<void>(ANumArguments);
+CYB::Engine::Helpers::CommandLine::CommandLine(const unsigned int ANumArguments, const oschar_t* const* const AArguments) :
+	FTokens(ParseTokens(ANumArguments, AArguments))
+{}
+
+CYB::API::Container::Vector<CYB::Engine::Helpers::CommandLine::Token> CYB::Engine::Helpers::CommandLine::ParseTokens(const unsigned int ANumArguments, const oschar_t* const* const AArguments) {
+	CYB::API::Container::Vector<CYB::Engine::Helpers::CommandLine::Token> Result;
+	Result.reserve(ANumArguments + 1);
+
+	using OSCharConverter =
+#ifdef TARGET_OS_WINDOWS	//Do not do this anywhere else in the Engine tree
+		API::String::UTF16;
+#else
+		API::String::UTF8;
+#endif
+
+	//Make no assumptions about argv[0]
 	static_cast<void>(AArguments);
+
+	return Result;
 }
